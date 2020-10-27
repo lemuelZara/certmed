3cdc04d10b8477efe103def5626e44ac0de1dde2
lemuelZara <lemuel.czara@gmail.com>
2020-10-27 16:48:55 -0300

    creation of type migration

---
+++ server/src/database/migrations/1603806624534-CreateTypes.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTypes1603806624534 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'types',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('types');
+  }
+}
