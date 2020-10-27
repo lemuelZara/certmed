c37a89ccfd8d181bca8752def1a0a5d28dcbbcb9
lemuelZara <lemuel.czara@gmail.com>
2020-10-27 16:48:37 -0300

    creation of user migration

---
+++ server/src/database/migrations/1603818108749-CreateUsers.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1603818108749 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'type_id',
+            type: 'integer',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_users_types',
+            columnNames: ['type_id'],
+            referencedTableName: 'types',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
