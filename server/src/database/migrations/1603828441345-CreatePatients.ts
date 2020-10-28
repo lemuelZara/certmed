dbc16e0bf516e596704984f7c759148a5581b4ca
lemuelZara <lemuel.czara@gmail.com>
2020-10-28 14:44:47 -0300

    create of patient migration

---
+++ server/src/database/migrations/1603828441345-CreatePatients.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePatients1603828441345 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'patients',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'user_id',
+            type: 'integer',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_patients_users',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('patients');
+  }
+}
