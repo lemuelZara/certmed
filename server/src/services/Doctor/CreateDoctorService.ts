3c028f9dd388f723d0efff97daf0852c5be7bcf2
lemuelZara <lemuel.czara@gmail.com>
2020-10-28 17:56:26 -0300

    creation of the method to create a new doctor

---
+++ server/src/services/Doctor/CreateDoctorService.ts
@@ -0,0 +1,59 @@
+import { Doctor } from '../../entities/Doctor';
+import AppError from '../../errors/AppError';
+import { BCryptHashProvider } from '../../providers/HashProvider/BCryptHashProvider';
+import IHashProvider from '../../providers/HashProvider/IHashProvider';
+import { DoctorRepository } from '../../repositories/Doctor/DoctorRepository';
+import IDoctorRepository from '../../repositories/Doctor/IDoctorRepository';
+import ITypeRepository from '../../repositories/Type/ITypeRepository';
+import { TypeRepository } from '../../repositories/Type/TypeRepository';
+import IUserRepository from '../../repositories/User/IUserRepository';
+import { UserRepository } from '../../repositories/User/UserRepository';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class CreateDoctorService {
+  private doctorRepository: IDoctorRepository;
+
+  private userRepository: IUserRepository;
+
+  private typeRepository: ITypeRepository;
+
+  private hashProvider: IHashProvider;
+
+  constructor(
+    doctorRepository: DoctorRepository,
+    userRepository: UserRepository,
+    typeRepository: TypeRepository,
+    hashProvider: BCryptHashProvider,
+  ) {
+    this.doctorRepository = doctorRepository;
+    this.userRepository = userRepository;
+    this.typeRepository = typeRepository;
+    this.hashProvider = hashProvider;
+  }
+
+  public async execute({ name, email, password }: Request): Promise<Doctor> {
+    const findType = await this.typeRepository.findByTypeName('Médico');
+
+    if (findType === undefined) {
+      throw new AppError('O tipo de usuário não existe!');
+    }
+
+    const hashPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password: hashPassword,
+      type: findType,
+    });
+
+    const doctor = await this.doctorRepository.create({ user });
+
+    return doctor;
+  }
+}
