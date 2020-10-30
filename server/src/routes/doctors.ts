bcd1ac163951a57af5b4a3c14e992f4d5967f479
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 16:51:25 -0300

    validation request body

--- server/src/routes/doctors.ts
+++ server/src/routes/doctors.ts
@@ -1,10 +1,30 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import { DoctorController } from '../controllers/DoctorController';
+
+import ensureAuthentication from '../middlewares/EnsureAuthentication';
+import authenticationDoctor from '../middlewares/AuthenticationDoctor';
 
 const doctorRoutes = Router();
 
 const doctorController = new DoctorController();
 
-doctorRoutes.post('/doctors', doctorController.create);
+doctorRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    },
+  }),
+  doctorController.create,
+);
+
+doctorRoutes.use(ensureAuthentication);
+doctorRoutes.get('/profile', authenticationDoctor, (req, res) => {
+  return res.json(req.user);
+});
 
 export { doctorRoutes };
--- server/src/routes/patients.ts
+++ server/src/routes/patients.ts
@@ -1,10 +1,30 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import { PatientController } from '../controllers/PatientController';
+
+import ensureAuthentication from '../middlewares/EnsureAuthentication';
+import authenticationPatient from '../middlewares/AuthenticationPatient';
 
 const patientRoutes = Router();
 
 const patientController = new PatientController();
 
-patientRoutes.post('/patients', patientController.create);
+patientRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    },
+  }),
+  patientController.create,
+);
+
+patientRoutes.use(ensureAuthentication);
+patientRoutes.get('/profile', authenticationPatient, (_, res) => {
+  return res.json({ ok: true });
+});
 
 export { patientRoutes };
