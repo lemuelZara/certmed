f1f3a02131a5070db0ab02e6037718cf4b101a7b
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 16:51:58 -0300

    update path routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { typeRoutes } from './types';
 import { doctorRoutes } from './doctors';
 import { patientRoutes } from './patients';
+import { sessionsRoutes } from './sessions';
 
 const routes = Router();
 
-routes.use(typeRoutes);
-routes.use(doctorRoutes);
-routes.use(patientRoutes);
+routes.use('/sessions', sessionsRoutes);
+routes.use('/types', typeRoutes);
+routes.use('/doctors', doctorRoutes);
+routes.use('/patients', patientRoutes);
 
 export { routes };
