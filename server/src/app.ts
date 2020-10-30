44ab6e8ba54709751a992e179ca2e1540dbf8459
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 16:59:59 -0300

    error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import './database';
 
 import express, { NextFunction, Request, Response } from 'express';
+import { errors } from 'celebrate';
 
 import { routes } from './routes';
 import AppError from './errors/AppError';
@@ -10,19 +12,18 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response
-        .status(err.statusCode)
-        .json({ status: 'error', message: err.message });
-    }
+app.use(errors());
 
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal Server Error' });
+});
 
 export { app };
