87995430b4105d5de568bbade203b8e199afa2db
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 17:04:42 -0300

    adding an email validation

--- server/src/services/Patient/CreatePatientService.ts
+++ server/src/services/Patient/CreatePatientService.ts
@@ -1,11 +1,16 @@
 import { Patient } from '../../entities/Patient';
+
 import AppError from '../../errors/AppError';
+
 import { BCryptHashProvider } from '../../providers/HashProvider/BCryptHashProvider';
 import IHashProvider from '../../providers/HashProvider/IHashProvider';
+
 import IPatientRepository from '../../repositories/Patient/IPatientRepository';
 import { PatientRepository } from '../../repositories/Patient/PatientRepository';
+
 import ITypeRepository from '../../repositories/Type/ITypeRepository';
 import { TypeRepository } from '../../repositories/Type/TypeRepository';
+
 import IUserRepository from '../../repositories/User/IUserRepository';
 import { UserRepository } from '../../repositories/User/UserRepository';
 
@@ -38,9 +43,14 @@ export class CreatePatientService {
 
   public async execute({ name, email, password }: Request): Promise<Patient> {
     const findType = await this.typeRepository.findByTypeName('Paciente');
+    const emailIsBeingUsed = await this.userRepository.findByEmail(email);
 
     if (findType === undefined) {
       throw new AppError('O tipo de usuário não existe');
+    }
+
+    if (emailIsBeingUsed) {
+      throw new AppError('O Email já está sendo usado!');
     }
 
     const hashPassword = await this.hashProvider.generateHash(password);
