02b33abf517536f2184b03f53eb6318135e40538
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 17:06:15 -0300

    updating the route path

--- server/src/routes/types.ts
+++ server/src/routes/types.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+
 import { TypeController } from '../controllers/TypeController';
 
 const typeRoutes = Router();
 
 const typeController = new TypeController();
 
-typeRoutes.post('/types', typeController.create);
-typeRoutes.get('/types', typeController.index);
+typeRoutes.post('/', typeController.create);
+typeRoutes.get('/', typeController.index);
 
 export { typeRoutes };
