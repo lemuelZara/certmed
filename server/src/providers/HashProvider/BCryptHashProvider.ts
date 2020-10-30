5e0b4da5b11cc25f8239fdb0a3303e09c0de7652
lemuelZara <lemuel.czara@gmail.com>
2020-10-30 16:56:45 -0300

    implementation of the method to compare passwords

--- server/src/providers/HashProvider/BCryptHashProvider.ts
+++ server/src/providers/HashProvider/BCryptHashProvider.ts
@@ -1,9 +1,13 @@
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
 import IHashProvider from './IHashProvider';
 
 export class BCryptHashProvider implements IHashProvider {
   public async generateHash(password: string): Promise<string> {
     return hash(password, 8);
+  }
+
+  public async compareHash(password: string, hashed: string): Promise<boolean> {
+    return compare(password, hashed);
   }
 }
