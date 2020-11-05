446c4655d3e425fde8d68b8d0003d39cba5a313a
lemuelZara <lemuel.czara@gmail.com>
2020-11-05 15:54:38 -0300

    appointment routes

--- server/src/routes/doctors.ts
+++ server/src/routes/doctors.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
 
 import { DoctorController } from '../controllers/DoctorController';
+import { AppointmentController } from '../controllers/AppointmentController';
 
 import ensureAuthentication from '../middlewares/EnsureAuthentication';
 import authenticationDoctor from '../middlewares/AuthenticationDoctor';
 
+import storageConfig from '../config/storage';
+
 const doctorRoutes = Router();
+const upload = multer(storageConfig.multer);
 
 const doctorController = new DoctorController();
+const appointmentController = new AppointmentController();
 
 doctorRoutes.post(
   '/',
@@ -26,5 +32,12 @@ doctorRoutes.use(ensureAuthentication);
 doctorRoutes.get('/profile', authenticationDoctor, (req, res) => {
   return res.json(req.user);
 });
+doctorRoutes.post(
+  '/appointments',
+  authenticationDoctor,
+  upload.single('document'),
+  appointmentController.create,
+);
+doctorRoutes.get('/appointments');
 
 export { doctorRoutes };
