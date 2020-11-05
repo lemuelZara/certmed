b314c28cf977b739195144f1586db86869843b13
lemuelZara <lemuel.czara@gmail.com>
2020-11-05 12:12:23 -0300

    document migration

---
+++ server/src/database/migrations/1604495411423-CreateDocuments.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateDocuments1604495411423 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'documents',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'path',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('documents');
+  }
+}
